4ff1bb8231f5d1f9c643b99abd2d8bdcc3f24cc7
Julio Cesar <vcjuliocesar@outlook.com>
2018-04-29 22:24:34 -0500

    inicialización del proyecto

---
+++ app/HtmlElement.php
@@ -0,0 +1,10 @@
+<?php
+namespace App;
+
+class HtmlElement
+{
+    public function render()
+    {
+        return "HTML ELEMENT HERE";
+    }
+}
---
+++ public/index.php
@@ -0,0 +1,6 @@
+<?php
+require '../vendor/autoload.php';
+
+$element = new \App\HtmlElement;
+
+echo $element->render();
