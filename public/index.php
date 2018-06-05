20f5b95f4e5de3d3f81bd106af7269d9853688af
Julio Cesar <vcjuliocesar@outlook.com>
2018-06-05 18:54:26 -0500

    add open,content,close and is void methods to HtmlElement

--- public/index.php
+++ public/index.php
@@ -1,6 +1,34 @@
 <?php
 require '../vendor/autoload.php';
 
-$element = new \App\HtmlElement;
+//$element = new \App\HtmlElement;
+$element = new \App\HtmlElement('p',[],'Este es el contenido');
+echo htmlentities($element->render(),ENT_QUOTES,'utf-8');
 
-echo $element->render();
+echo "<br><br>";
+
+$element = new \App\HtmlElement('p',['id' => 'my_paragraph'],'Este es el contenido');
+echo htmlentities($element->render(),ENT_QUOTES,'utf-8');
+
+echo "<br><br>";
+
+$element = new \App\HtmlElement('p',['id' => 'my_paragraph', 'class' => 'paragraph'],'Este es el contenido');
+echo htmlentities($element->render(),ENT_QUOTES,'utf-8');
+
+echo "<br><br>";
+
+$element = new \App\HtmlElement('img',['src' => 'img/styde.png']);
+echo htmlentities($element->render(),ENT_QUOTES,'utf-8');
+
+echo "<br><br>";
+
+$element = new \App\HtmlElement('img',['src' => 'img/styde.png','title' => 'Curso de "Refactorización" en Styde']);
+echo htmlentities($element->render(),ENT_QUOTES,'utf-8');
+
+echo "<br><br>";
+
+
+$element = new \App\HtmlElement('input', ['required']);
+echo htmlentities($element->render(),ENT_QUOTES,'utf-8');
+
+echo "<br><br>";
