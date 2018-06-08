0b6c99e1da99973af4288ab1ddc4ee8917cbbb9e
Julio Cesar <vcjuliocesar@outlook.com>
2018-06-07 21:56:52 -0500

    Try methods open and close

--- app/HtmlElement.php
+++ app/HtmlElement.php
@@ -16,8 +16,21 @@ class HtmlElement
 
     public function render()
     {
-        //return "HTML ELEMENT HERE";
-        //si el elemento tiene atributos
+        $result = $this->open();
+        
+        if($this->isVoid()) {
+            return $result;
+        }
+
+        $result .= $this->content();
+
+        $result .= $this->close();
+
+        return $result;
+    }
+
+    public function open() : string
+    {
         if(! empty($this->attributes)) {
             $htmlAttributes ='';
 
@@ -35,17 +48,22 @@ class HtmlElement
             // Abrir la etiqueta sin atributos
             $result = '<'.$this->name.'>';
         }
-        // Si el elemento es void
 
-        if(in_array($this->name, ['br', 'hr', 'img', 'input', 'meta'])) {
-            return $result;
-        }
-
-        // Imprimir el contenido
-        $result .= htmlentities($this->content, ENT_QUOTES, 'UTF-8');
-
-        // Cerrar la etiqueta
-        $result .= '</'.$this->name.'>';
         return $result;
+    }
+
+    public function isVoid() : bool
+    {
+        return in_array($this->name, ['br', 'hr', 'img', 'input', 'meta']);
+    }
+
+    public function content() : string
+    {
+        return htmlentities($this->content, ENT_QUOTES, 'UTF-8');
+    }
+
+    public function close() : string
+    {
+        return '</'.$this->name.'>';
     }
 }
