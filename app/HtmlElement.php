0edc4ada007867e9c6730e2b7058f6d7c22c6493
Julio Cesar <vcjuliocesar@outlook.com>
2018-06-12 14:48:42 -0500

    delete temporaty variables

--- app/HtmlElement.php
+++ app/HtmlElement.php
@@ -16,17 +16,12 @@ class HtmlElement
 
     public function render()
     {
-        $result = $this->open();
         
         if($this->isVoid()) {
-            return $result;
+            return $this->open();
         }
 
-        $result .= $this->content();
-
-        $result .= $this->close();
-
-        return $result;
+        return $this->open().$this->content().$this->close();
     }
 
     public function open() : string
@@ -45,22 +40,18 @@ class HtmlElement
 
     public function attributes() : string
     {
-        $htmlAttributes='';
-
-        foreach($this->attributes as $attribute => $value) {
-            $htmlAttributes.= $this->renderAttributes($attribute,$value);
-        }
-
-        return $htmlAttributes;
+        return array_reduce(array_keys($this->attributes), function ($result, $attribute) {
+            return $result. $this->renderAttribute($attribute);
+        },'');
     }
 
-    protected function renderAttributes($attribute,$value)
+    protected function renderAttribute($attribute)
     {
         if(is_numeric($attribute)) {
-            return ' '.$value;
+            return ' '.$this->attributes[$attribute];
         }
 
-        return ' '.$attribute.'="'.htmlentities($value, ENT_QUOTES, 'UTF-8').'"'; // name="value";
+        return ' '.$attribute.'="'.htmlentities($this->attributes[$attribute], ENT_QUOTES, 'UTF-8').'"'; // name="value";
     }
 
     public function isVoid() : bool
