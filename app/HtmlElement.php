3b735074da229ab41d9715db9077e234723f2d60
Julio Cesar <vcjuliocesar@outlook.com>
2018-06-12 16:18:17 -0500

    add HtmlAttributes class

--- app/HtmlElement.php
+++ app/HtmlElement.php
@@ -10,7 +10,7 @@ class HtmlElement
     public function __construct(string $name,$attributes = [], $content = null)
     {
         $this->name = $name;
-        $this->attributes = $attributes;
+        $this->attributes = new HtmlAttributes($attributes);
         $this->content = $content;
     }
 
@@ -26,32 +26,12 @@ class HtmlElement
 
     public function open() : string
     {
-        if($this->hasAttributes()) {
-            return '<'.$this->name.$this->attributes().'>';
-        } else {
-            return '<'.$this->name.'>';
-        }
-    }
-
-    public function hasAttributes() : bool
-    {
-        return ! empty($this->attributes);
+        return '<'.$this->name.$this->attributes().'>';
     }
 
     public function attributes() : string
     {
-        return array_reduce(array_keys($this->attributes), function ($result, $attribute) {
-            return $result. $this->renderAttribute($attribute);
-        },'');
-    }
-
-    protected function renderAttribute($attribute)
-    {
-        if(is_numeric($attribute)) {
-            return ' '.$this->attributes[$attribute];
-        }
-
-        return ' '.$attribute.'="'.htmlentities($this->attributes[$attribute], ENT_QUOTES, 'UTF-8').'"'; // name="value";
+        return $this->attributes->render();
     }
 
     public function isVoid() : bool
